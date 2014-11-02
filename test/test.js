365b1f760830d26f3b608b6b091e8dbe4f071053
Jose Antonio Rodríguez <joseantonio.rodriguezfernandez@telefonica.com>
2014-11-02 13:29:21 +0100

    1-new-keywords

---
+++ test/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var tartare = require('tartare');
+
+
+feature('Get users using the API', 'I am able to use the Users API to read the users of the SUT', function() {
+  scenario('Get a user that exists in the SUT', function() {
+    given('there is a user with username john in the SUT', function() {
+
+    });
+    when('I use the API to get the user looking up by his name', function() {
+
+    });
+    then('I get a 200 OK HTTP Status Code', function() {
+
+    });
+    and('the HTTP body has the expected user data', function() {
+
+    });
+  });
+});
